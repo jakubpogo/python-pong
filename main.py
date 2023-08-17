82531daaf56ec047284d41d934ae5767b5f2482e
jakubpogo <jakubpogo@users.noreply.github.com>
2023-08-17 18:58:55 +0100

    imported pygame and created a screen

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-msg = "Hello World"
-print(msg)
+import pygame
+
+pygame.init()
+
+#set up the display
+
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Pong Game")
