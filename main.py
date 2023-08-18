ca72f16c70213201a1128fb6a13ab1cebd5975f4
jakubpogo <jakubpogo@users.noreply.github.com>
2023-08-18 11:58:56 +0100

    Set up the pygame window

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 pygame.init()
 
@@ -6,4 +7,32 @@ pygame.init()
 
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Pong Game")
+pygame.display.set_caption("Pong")
+
+FPS = 60
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+def draw(win):
+    win.fill(black)
+    pygame.display.update()
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+        draw(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+    
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
+
